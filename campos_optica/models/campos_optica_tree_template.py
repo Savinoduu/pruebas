6f42d09e4bd69c5b1c14f66e83b9f58bdde7d45d
Savi Dueñas <savi@noduu.com>
2022-06-07 11:53:42 -0600

    actualizando vista de arbol

--- campos_optica/models/campos_optica_tree_template.py
+++ campos_optica/models/campos_optica_tree_template.py
@@ -5,7 +5,7 @@ from odoo import models, fields
 
 
 class CamposOpticaTreeTemplate(models.Model):
-    _inherit = 'sale.order'
+    _inherit = 'sale.order', 'account.move'
 
     Campo_Laboratorio_Tree = fields.Many2one(comodel_name='res.partner',string="Laboratorio",required=False, ondelete='cascade')
     Campo_Paciente_Tree = fields.Many2one(comodel_name='res.partner',string="Paciente",required=False, ondelete='cascade')
